d5cd1523e6c1c79e3eaea60a4e17d3ebb3f5d592
unknown <michaeladdis78@gmail.com>
2025-04-30 15:18:04 +0300

    intentionally failing tests

--- sum.test.js
+++ sum.test.js
@@ -5,5 +5,5 @@ const sum = require('./sum');
 // Simple tests
 console.log(sum(1, 2) === 3 ? 'Test 1 Passed' : 'Test 1 Failed');
 console.log(sum(-5, 5) === 0 ? 'Test 2 Passed' : 'Test 2 Failed');
-console.log(sum(10, 15) === 23 ? 'Test 3 Passed' : 'Test 3 Failed');
+console.log(sum(10, 15) === 223 ? 'Test 3 Passed' : 'Test 3 Failed');
 console.log(sum(0, 0) === 1 ? 'Test 4 Passed' : 'Test 4 Failed');
